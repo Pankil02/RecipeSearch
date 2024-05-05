2553c4fbaa1db8d36b0196bf5b811f2efd311a83
Pankil02 <Pankil1702@gmail.com>
2024-05-05 16:16:48 -0400

    Setting up the Crud Operation with Database and Favourite Recipes functionality.

--- app/src/main/java/com/example/recipesearch/RecipeFavorites.java
+++ app/src/main/java/com/example/recipesearch/RecipeFavorites.java
@@ -1,9 +1,7 @@
 package com.example.recipesearch;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -25,91 +23,83 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.room.Room;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
 import com.example.recipesearch.data.RecipeData;
 import com.example.recipesearch.data.RecipeDataDAO;
 import com.example.recipesearch.data.RecipeDatabase;
-import com.example.recipesearch.databinding.ActivityMainBinding;
+import com.example.recipesearch.databinding.ActivityRecipeFavoritesBinding;
 import com.example.recipesearch.ui.RecipeSearchViewModel;
 import com.google.android.material.snackbar.Snackbar;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
-public class MainActivity extends AppCompatActivity {
+public class RecipeFavorites extends AppCompatActivity {
 
-    ActivityMainBinding binding;
-    RecipeDataDAO rDAO;
-    RecipeSearchViewModel recipeModel;
-    ArrayList<RecipeData> data = new ArrayList<RecipeData>();
     RecyclerView.Adapter myAdapter;
+    ActivityRecipeFavoritesBinding binding;
+    ArrayList<RecipeData> data;
+    //    SunData sunData = new SunData();
+    RecipeSearchViewModel recipeModel;
+    RecipeDataDAO rDAO;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_recipe_favorites);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ActivityRecipeFavoritesBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.recyclerviewMain.setLayoutManager(new LinearLayoutManager(this));
         setSupportActionBar(binding.toolbar);
 
+        binding.favouriteRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
         recipeModel = new ViewModelProvider(this).get(RecipeSearchViewModel.class);
-
-        if(data == null) {
-            recipeModel.Rdata.postValue( data = new ArrayList<RecipeData>());
-        }
-
-        data = recipeModel.Rdata.getValue();
-
         Executor thread = Executors.newSingleThreadExecutor();
         thread.execute(() ->
         {
             RecipeDatabase db = Room.databaseBuilder(getApplicationContext(), RecipeDatabase.class, "Recipes-Database").build();
             rDAO = db.rDAO();
-            data = new ArrayList<>();
-            data.addAll( rDAO.getAllData());
-            runOnUiThread( () ->
-                    binding.recyclerviewMain.setAdapter( myAdapter ));
+
+            data.addAll(rDAO.getAllData()); //Once you get the data from database
+
+            runOnUiThread(() -> {
+                recipeModel.Rdata.postValue(data);
+                binding.favouriteRecyclerView.setAdapter(myAdapter);
+                myAdapter.notifyDataSetChanged();
+            });
         });
 
-        myAdapter= new RecyclerView.Adapter<MyRowHolder>() {
+        data = recipeModel.Rdata.getValue();
+
+        if(data == null)
+        {
+            recipeModel.Rdata.postValue( data = new ArrayList<RecipeData>());
+        }
+
+        myAdapter = new RecyclerView.Adapter<RecipeFavorites.MyRowHolder>() {
             @NonNull
             @Override
-            public MyRowHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            public RecipeFavorites.MyRowHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
                 View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recipe_show, parent, false);
                 // Create and return a new instance of MyRowHolder with the inflated layout
                 return new MyRowHolder(view);
             }
 
-            @SuppressLint("CheckResult")
             @Override
-            public void onBindViewHolder(@NonNull MyRowHolder holder, int position) {
-                RecipeData obj =data.get(position);
+            public void onBindViewHolder(@NonNull RecipeFavorites.MyRowHolder holder, int position) {
+                RecipeData obj = data.get(position);
                 holder.recipeTitle.setText(String.valueOf(obj.getTitle()));
                 holder.recipeId.setText(String.valueOf(obj.getItemId()));
-
                 Glide.with(holder.itemView.getContext())
                         .load(obj.getImageUrl())
                         .into(holder.recipeImage);
-//
             }
 
             @Override
@@ -118,56 +108,9 @@ public class MainActivity extends AppCompatActivity {
             }
 
         };
-        binding.recyclerviewMain.setAdapter(myAdapter);
-        myAdapter.notifyDataSetChanged();
+        binding.favouriteRecyclerView.setAdapter(myAdapter);
 
-
-        binding.button.setOnClickListener(c->{
-            recipeModel.Rdata.setValue(data);
-            myAdapter.notifyDataSetChanged();
-        });
-        getRecipes();
     }
-
-    public void getRecipes() {
-
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url="https://api.spoonacular.com/recipes/complexSearch?query=" + binding.searchText.getText().toString() + "&apiKey=75601041d8844b8fa58a85e5572a78c0";
-
-        JsonObjectRequest stringRequest = new JsonObjectRequest(Request.Method.GET, url,null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            JSONArray results=response.getJSONArray("results");
-                            data = new ArrayList<RecipeData>();
-                            for(int i=0;i<results.length();i++) {
-
-                                JSONObject recipeObject =results.getJSONObject(i);
-
-                                String iteemid = recipeObject.getString("id");
-                                String title1 = recipeObject.getString("title");
-                                String imageUrl1 = recipeObject.getString("image");
-
-                                RecipeData singleData = new RecipeData(iteemid, title1, imageUrl1);
-
-                                data.add(singleData);
-                            }
-                        } catch (JSONException e) {
-                            Log.e("JSON Parse Error", "Error parsing JSON response: " + e.getMessage());
-                            throw new RuntimeException(e);
-                        }
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(MainActivity.this,"Error in Fecting Api..",Toast.LENGTH_SHORT).show();
-                Log.e("api","onErrorResponse: "+ error.getLocalizedMessage());
-            }
-        });
-        queue.add(stringRequest);
-    }
-
     public class MyRowHolder extends RecyclerView.ViewHolder {
         TextView recipeTitle;
         TextView recipeId;
@@ -183,7 +126,7 @@ public class MainActivity extends AppCompatActivity {
             itemView.findViewById(R.id.deleteIcon).setOnClickListener(clk->{
                 int position = getAbsoluteAdapterPosition();
 
-                AlertDialog.Builder builder = new AlertDialog.Builder( MainActivity.this );
+                AlertDialog.Builder builder = new AlertDialog.Builder( RecipeFavorites.this );
                 builder.setMessage("You want to delete this recipe")
                         .setTitle("Question: ")
                         .setNegativeButton ("No", (dialog, cl) -> {})
@@ -192,6 +135,10 @@ public class MainActivity extends AppCompatActivity {
                             data.remove(position);
                             myAdapter.notifyItemRemoved(position);
 
+                            Executor thread = Executors.newSingleThreadExecutor();
+                            thread.execute(() -> {
+                                removeRecipe.id =  rDAO.deleteMessage(removeRecipe);
+                            });
 
                             Snackbar.make(recipeId,"You deleted recipe # "+ position,Snackbar.LENGTH_LONG)
                                     .setAction("Undo", click-> {
@@ -208,39 +155,20 @@ public class MainActivity extends AppCompatActivity {
                         .create().show();
 
             });
-            itemView.findViewById(R.id.fav).setOnClickListener(p->{
-                int position = getAbsoluteAdapterPosition();
-                RecipeData favoriteData = data.get(position);
-
-                try {
-                    Executor thread = Executors.newSingleThreadExecutor();
-                    thread.execute(() -> {
-                        favoriteData.id = (int) rDAO.insertMessage(favoriteData);
-                        runOnUiThread(() -> {
-                            Toast.makeText(MainActivity.this, " Added to the favorite list and database.",
-                                            Toast.LENGTH_SHORT)
-                                    .show();
-                        });
-                    });
-                }catch (Exception e) {
-                    // Handle the exception, e.g., log it or show an error message
-                    Log.e("Database Insertion Error", "Error inserting data into the database: " + e.getMessage());
-                    runOnUiThread(() -> {
-                        Toast.makeText(MainActivity.this, "An error occurred while saving to the database because its already there", Toast.LENGTH_SHORT).show();
-                    });
-                }
-
-            });
 
             itemView.setOnClickListener(p->{
                 int position = getAbsoluteAdapterPosition();
                 RecipeData rr = data.get(position);
                 String idd= rr.getItemId();
-                Intent intent = new Intent(MainActivity.this, RecipeSummary.class);
+                Intent intent = new Intent(RecipeFavorites.this, RecipeSummary.class);
+
                 intent.putExtra("id",idd);
+
                 startActivity(intent);
 
             });
+
+
 
         }
     }
@@ -251,10 +179,13 @@ public class MainActivity extends AppCompatActivity {
     }
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // Delete all messages from the database
         if(item.getItemId() ==R.id.delIcon){
 
-            AlertDialog.Builder builder = new AlertDialog.Builder( MainActivity.this );
-            builder.setMessage("Are You sure you want to delete all saved Recipes From database")
+            AlertDialog.Builder builder = new AlertDialog.Builder( RecipeFavorites.this );
+
+
+            builder.setMessage("Are You sure you want to delete all saved data From database")
                     .setTitle("DELETE")
                     .setNegativeButton ("No", (dialog, cl) -> {})
                     .setPositiveButton ( "Yes", (dialog, cl) -> {
@@ -266,7 +197,7 @@ public class MainActivity extends AppCompatActivity {
                             runOnUiThread(() -> {
                                 data.clear();
                                 myAdapter.notifyDataSetChanged();
-                                Toast.makeText(MainActivity.this, "All messages deleted from the database", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(RecipeFavorites.this, "All messages deleted from the database", Toast.LENGTH_SHORT).show();
                             });
                         });
                     })
@@ -274,23 +205,18 @@ public class MainActivity extends AppCompatActivity {
 
             return true;
 
-        }else if(item.getItemId() ==R.id.helpIcon){
+        } else if(item.getItemId() ==R.id.helpIcon){
 
-            AlertDialog.Builder builder = new AlertDialog.Builder( MainActivity.this );
+            AlertDialog.Builder builder = new AlertDialog.Builder( RecipeFavorites.this );
+
             builder.setMessage("Welcome to our food search app! Use the search bar to find delicious recipes. " +
-                            "Tap on a recipe to view its details. Swipe left to delete recipes you no longer need. Enjoy exploring new culinary delights!")
+                            "Tap on a recipe to view its details. Swipe left to delete recipes you no longer need. Enjoy exploring new culinary delights!" )
                     .setTitle("Helpful")
                     .setNegativeButton ("No", (dialog, cl) -> {})
                     .setPositiveButton ( "Yes", (dialog, cl) -> {})
                     .create().show();
-            return true;
-        }else if(item.getItemId() ==R.id.favorites){
-            Intent nextPage = new Intent(MainActivity.this, RecipeFavorites.class);
-            startActivity(nextPage);
-            return true;
         }
         return true;
-
     }
 
 
--- app/src/main/java/com/example/recipesearch/RecipeSummary.java
+++ app/src/main/java/com/example/recipesearch/RecipeSummary.java
@@ -7,9 +7,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.room.Room;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
+import com.example.recipesearch.databinding.ActivityRecipeSummaryBinding;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class RecipeSummary extends AppCompatActivity {
 
+    ActivityRecipeSummaryBinding binding;
+    String image;
+    String summary ;
+    String urlT;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +41,60 @@ public class RecipeSummary extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        binding = ActivityRecipeSummaryBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        String idd = getIntent().getStringExtra("id");
+        getRecipeSummary(idd);
+
+
+        binding.button.setOnClickListener(c->{
+            binding.summaryText.setText(summary);
+            binding.Url.setText(urlT);
+            Glide.with(this)
+                    .load(image)
+                    .into(binding.imageBig);
+
+        });
+    }
+    public void getRecipeSummary(String idd ){
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url="https://api.spoonacular.com/recipes/"+ idd +"/information?apiKey=75601041d8844b8fa58a85e5572a78c0";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            String imageO = jsonObject.getString("image");
+                            String smry = jsonObject.getString("summary");
+                            String srl = jsonObject.getString("spoonacularSourceUrl");
+
+                            image = imageO;
+                            summary =smry;
+                            urlT=srl;
+
+                                binding.summaryText.setText(summary);
+                                binding.Url.setText(urlT);
+                                Glide.with(RecipeSummary.this)
+                                        .load(image)
+                                        .into(binding.imageBig);
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        queue.add(stringRequest);
+
     }
 }
